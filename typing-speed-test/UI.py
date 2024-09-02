2543616d0dc81c2a89d441c23c25db0b185efd6d
Yadu <yadusasankan@gmail.com>
2024-09-02 21:05:56 +0530

    feat(typing-test): load text and display in ui

--- typing-speed-test/UI.py
+++ typing-speed-test/UI.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+import json
+import random
 
 
 class UI:
@@ -8,6 +10,7 @@ class UI:
 
         self.root = Tk()
         self.mainframe = None
+        self.sample_text = None
         self.configure_ui()
 
     def configure_ui(self):
@@ -20,23 +23,33 @@ class UI:
         mainframe = ttk.Frame(root, padding="50 50 60 60")
         mainframe.grid(column=0, row=0)
 
+        sample_text = StringVar()
+        sample_text.set("sample text here")
+
         text_label = Label(
             mainframe,
-            text="sample text here",
+            textvariable=sample_text,
             borderwidth=2,
             relief="sunken",
-            width=30,
-            height=5,
+            width=60,
+            height=15,
+            wraplength=340,
         )
         text_label.grid(column=0, row=0, columnspan=2, sticky="ew")
 
         self.mainframe = mainframe
+        self.sample_text = sample_text
 
     def launch_ui(self):
+        self.load_words()
         self.root.mainloop()
 
     def listen_keystroke(self):
         pass
 
     def load_words(self):
-        pass
+        with open("./typing-speed-test/common-words.json") as file:
+            all_words = json.load(file)
+            selected_words = random.choices(all_words, k=100)
+            paragraph = " ".join(selected_words)
+            self.sample_text.set(paragraph)
