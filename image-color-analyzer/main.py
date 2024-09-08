53f22c7a04a1cd2719e03bd8b3ea808f9cd31730
Yadu <yadusasankan@gmail.com>
2024-09-08 22:04:52 +0530

    chore: add TODO's to expand scope in future

--- image-color-analyzer/main.py
+++ image-color-analyzer/main.py
@@ -3,6 +3,9 @@ import numpy as np
 from typing import List
 
 
+COLOR_DELTA = 24
+
+
 def convert_to_hex(rgb_list: List[int]):
     r, g, b = rgb_list
     hex_code = f"#{r:02X}{g:02X}{b:02X}"
@@ -20,7 +23,11 @@ def find_top_colors(hex_array: List[str]):
     # sorted_counts = counts[sorted_indices]
     # print(f"sorted_counts: {sorted_counts[:10]}")
 
-    # TODO: 1. calculated euclidean distance between colors and shrink array based on a delta value
+    # TODO: 2. calculated euclidean distance between colors and shrink array
+    # based on a delta value doing this manually using euclidean distance maybe
+    # somewhat similar to data aggregation using something like k means
+    # clustering and would require/benefit using some library that can do that
+    # directly instead of having to code it out in numpy
 
     # return top 10 sorted values
     return sorted_values[:10]
@@ -35,7 +42,13 @@ def main():
     print(f"resolution: {width} x {height}")
 
     # convert and flatten to a single dimensional array and convert each rgb to hex
-    hex_colors = [convert_to_hex(item) for row in img_ndarray for item in row]
+    colors = [
+        (convert_to_hex(item), (item[0], item[1], item[2]))
+        for row in img_ndarray
+        for item in row
+    ]
+
+    hex_colors = [color[0] for color in colors]
     top_colors = find_top_colors(hex_colors)
 
     print(f"top_colors: {top_colors}")
@@ -45,7 +58,7 @@ if __name__ == "__main__":
     main()
 
 
-# TODO: 2. make GUI so that:
+# TODO: 1. make GUI so that:
 # a) images can be selected with a file selector
 # b) top 10 colors can be displayed in the UI visually alongside the image preview;
 # c) delta value for color variance can be input through ui and button to rerun calculations based on new delta
