cca8161c6810820c36b548dc9203dc1b17987943
Yadu <yadusasankan@gmail.com>
2024-09-10 12:48:53 +0530

    feat(color analyzer): display top 10 colors and corresponding colors strip in UI

--- image-color-analyzer/ui.py
+++ image-color-analyzer/ui.py
@@ -22,19 +22,21 @@ class UI:
         self.image_preview_label: ttk.Label = None
         self.load_image_btn: ttk.Button = None
         self.filename: str = None
+        self.color_frame: ttk.Frame = None
 
         self.config_main_ui()
 
     def config_main_ui(self):
 
-        # root and mainframe
+        # root
         self.root.title("Image Analyzer")
         root = self.root
         root.geometry("500x500")
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
-        mainframe = ttk.Frame(root, padding="50 50 60 60")
+        # mainframe
+        mainframe = ttk.Frame(root, padding="50 50 60 0")
         mainframe.grid(column=0, row=0)
         self.mainframe = mainframe
 
@@ -52,6 +54,10 @@ class UI:
         )
         load_image_btn.grid(column=0, row=2)
 
+        # Color table frame
+        color_frame = ttk.Frame(root, padding="50 10 50 50")
+        color_frame.grid(column=0, row=1)
+
         # hide image preview and name label(until image is loaded)
         image_preview_label.grid_remove()
         image_name_label.grid_remove()
@@ -60,6 +66,7 @@ class UI:
         self.image_name_label = image_name_label
         self.image_preview_label = image_preview_label
         self.load_image_btn = load_image_btn
+        self.color_frame = color_frame
 
     def start_ui(self):
         self.root.mainloop()
@@ -109,5 +116,16 @@ class UI:
         self.root.after(100, func=self.analyze_color)
 
     def analyze_color(self):
-        top_colors = self.color_processor.analyze_image()
-        print(f"top_colors: {top_colors}")
+        self.top_colors = self.color_processor.analyze_image()
+        self.display_colors()
+        # print(f"top_colors: {self.top_colors}")
+
+    def display_colors(self):
+        for i, color in enumerate(self.top_colors):
+            ttk.Label(self.color_frame, text=color, width=10).grid(row=i, column=0)
+            ttk.Label(self.color_frame, background=color, width=10).grid(
+                row=i, column=2
+            )
+
+        self.root.update_idletasks()
+        self.root.geometry("")
